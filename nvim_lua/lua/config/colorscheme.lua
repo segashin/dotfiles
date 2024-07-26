0009ae29a11c02aae6acad724d88c038d94b3105
segashin <segashin0810shin@outlook.jp>
2024-07-26 23:16:35 +0900

    changed diagnostics border color

--- nvim_lua/lua/config/colorscheme.lua
+++ nvim_lua/lua/config/colorscheme.lua
@@ -5,16 +5,16 @@ print('setting colorscheme ' .. colorscheme)
 if colorscheme == 'nightfox' or colorscheme == 'carbonfox' then
   require('nightfox').setup({
     options = {
-      dim_inactive = true,
+      -- dim_inactive = true,
     },
     groups = {
       nightfox = {
-        NormalFloat = { fg = 'none', bg = 'gray25' },
-        FloatBorder = { fg = 'green4', bg = 'gray25' }
+        NormalFloat = { fg = 'none', bg = 'gray18' },
+        FloatBorder = { fg = 'firebrick', bg = 'gray18' }
       },
       carbonfox = {
         NormalFloat = { fg = 'none', bg = 'gray20' },
-        FloatBorder = { fg = 'green4', bg = 'gray20' }
+        FloatBorder = { fg = 'HotPink4', bg = 'gray20' }
       },
     }
   })
