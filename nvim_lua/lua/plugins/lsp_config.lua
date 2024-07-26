68d0162ce6d9079b0f2dd8c1708f843ef1dda40c
segashin <segashin0810shin@outlook.jp>
2024-07-26 22:55:42 +0900

    auto completion added

--- nvim_lua/lua/plugins/lsp_config.lua
+++ nvim_lua/lua/plugins/lsp_config.lua
@@ -17,11 +17,14 @@ return {
     {
         "neovim/nvim-lspconfig",
         config = function()
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
             local lspconfig = require('lspconfig')
             local mason_lspconfig = require("mason-lspconfig")
             mason_lspconfig.setup_handlers({
                 function(server_name)
-                    lspconfig[server_name].setup({})
+                    lspconfig[server_name].setup({
+                        capabilities = capabilities
+                    })
                 end
             })
 
@@ -48,6 +51,69 @@ return {
                 }
               }
             }
+
+            require('vim.lsp.protocol').CompletionItemKind = {
+              '󰊄', -- Text
+              '󰬔', -- Method
+              '󰊕', -- Function
+              '', -- Constructor
+              '󰬍', -- Field
+              '󰬝', -- Variable
+              '', -- Class
+              '', -- Interface
+              '󰕳', -- Module
+              '', -- Property
+              '', -- Unit
+              '', -- Value
+              '', -- Enum
+              '', -- Keyword
+              '', -- Snippet
+              '', -- Color
+              '', -- File
+              '', -- Reference
+              '', -- Folder
+              '', -- EnumMember
+              '', -- Constant
+              '', -- Struct
+              '', -- Event
+              '', -- Operator
+              '', -- TypeParameter
+            }
+
+            -- Show border of the diagnostic floating window
+            vim.diagnostic.config({
+              float = { border = "single" }
+            })
+
+            -- Cofigure the diagnostic highlighting
+            vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+              vim.lsp.diagnostic.on_publish_diagnostics, {
+                underline = true,
+                update_in_insert = false,
+                virtual_text = { spacing = 4, prefix = "●", source = true },
+                severity_sort = true,
+              }
+            )
+
+            vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+              vim.lsp.handlers.hover, {
+                border = "single", -- "shadow", "none", "rounded"
+              }
+            )
+            -- The following settings are defined in /lua/myscripts/colorscheme.lua
+            -- - change NormalFloat color
+            -- - change FloatBorder color
+
+            -- The following settings are defined in /lua/myscripts/keymaps.lua
+            -- - keymap to show lsp.buf
+            -- - keymap to show diagnostics
+
+            -- Diagnostic symbols in the sign column (gutter)
+            local signs = { Error = "", Warn = "", Hint = "", Info = " " }
+            for type, icon in pairs(signs) do
+              local hl = "DiagnosticSign" .. type
+              vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+            end
         end
     }
 }
