7bf8486d353d38a55ab0d2ccb061485298c530d7
segashin <segashin0810shin@outlook.jp>
2024-10-18 22:09:56 +0900

    Added window management keymaps

--- nvim_lua/lua/config/keymaps.lua
+++ nvim_lua/lua/config/keymaps.lua
@@ -38,8 +38,8 @@ highlightCword(4, 'white', 'LemonChiffon1', false)
 highlightCword(5, 'black', 'bisque4', false)
 
 -- marker
-keymap.set('n', '<Leader>m', '`', {noremap = true, silent = true})
-
+keymap.set('n', '<Leader>n', ':<C-U>BufferPrevious<CR>')
+keymap.set('n', '<Leader>m', ':<C-U>BufferNext<CR>')
 
 -- write and quit
 keymap.set('n', '<Leader>ww', ':<C-u>w<CR>', {noremap = true, silent = true})
@@ -60,6 +60,9 @@ keymap.set('n', '<Leader>vs', ':<C-u>wincmd v<CR>', {noremap = true, silent = tr
 
 -- wincmd manage window
 keymap.set('n', '<Leader>oo', ':<C-u>wincmd o<CR>', {noremap = true, silent = true})
+keymap.set('n', '<Leader>wr', ':<C-u>wincmd r<CR>', {noremap = true, silent = true})
+keymap.set('n', '<Leader>wj', ':<C-u>wincmd J<CR>', {noremap = true, silent = true})
+keymap.set('n', '<Leader>wh', ':<C-u>wincmd H<CR>', {noremap = true, silent = true})
 
 -- tab
 -- keymap.set('n', '}', 'gt', {noremap = true, silent = true})
@@ -88,7 +91,7 @@ keymap.set("n", "<Leader>gf", vim.lsp.buf.format, {})
 -- keymap.set('n', '<Leader>jl', '<cmd>ju<CR>', {noremap = true, silent = true}) -- show jump list
 -- keymap.set('n', '<Leader>jn', '<C-O>', {noremap = true, silent = true}) -- next
 -- keymap.set('n', '<Leader>jb', '<C-I>', {noremap = true, silent = true}) -- back
--- keymap.set('n', '<Leader>jd', '<cmd>lua vim.lsp.buf.definition()<CR>', {noremap = true, silent = true}) -- to def
+keymap.set('n', '<Leader>jd', '<cmd>lua vim.lsp.buf.definition()<CR>', {noremap = true, silent = true}) -- to def
 -- keymap.set('n', '<Leader>ju', '<cmd>lua vim.diagnostic.goto_next()<CR>', {noremap = true, silent = true}) -- to next issue
 -- keymap.set('n', '<Leader>jy', '<cmd>lua vim.diagnostic.goto_prev()<CR>', {noremap = true, silent = true}) -- to prev issue
 
