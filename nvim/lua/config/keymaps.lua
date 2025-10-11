8f8d5cc386dd443ee38f3ba8e921de90c0ff0224
segashin <segashin0810shin@outlook.jp>
2025-10-11 17:36:33 -0400

    Update diffview config

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -116,7 +116,7 @@ keymap.set('n', '<leader>fv', ':<C-u>Telescope file_browser path=%:p:h slsect_bu
 keymap.set('n', '<leader>tt', ':Neotree filesystem toggle<CR>', {})
 
 -- Diffview
-keymap.set('n', '<leader>vo', ':DiffviewOpen<CR>', {})
+keymap.set('n', '<leader>vo', ':DiffviewToggle<CR>', {})
 keymap.set('n', '<leader>vq', ':DiffviewClose<CR>', {})
 -- keymap.set('n', '<leader>vbc', ':DiffviewFileHistory<CR>', {})
 -- keymap.set('n', '<leader>vfc', ':DiffviewFileHistory %<CR>', {})
--- nvim/lua/plugins/diffview.lua
+++ nvim/lua/plugins/diffview.lua
@@ -14,5 +14,29 @@ return {
                 }
             }
         })
+
+        -- Close diffview on qq or qa
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = { "DiffviewFiles", "DiffviewFilePanel" },
+            callback = function()
+                vim.keymap.set("n", "<leader>qq", "<cmd>DiffviewClose<cr>", { buffer = true })
+                vim.keymap.set("n", "<leader>qa", "<cmd>DiffviewClose<cr>", { buffer = true })
+            end,
+        })
+
+        -- Create DiffviewToggle function
+        vim.api.nvim_create_user_command("DiffviewToggle", function()
+            local ok, lib = pcall(require, "diffview.lib")
+            if not ok then
+                vim.cmd("DiffviewOpen")
+                return
+            end
+            local view = lib.get_current_view()
+            if view then
+                vim.cmd("DiffviewClose")
+            else
+                vim.cmd("DiffviewOpen")
+            end
+        end, {})
     end,
 }
