2ad2dfdf17d163738404347c6a59782e9c7cf38e
segashin <segashin0810shin@outlook.jp>
2025-11-27 17:32:00 -0500

    Fix diff colors

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -7,16 +7,32 @@ return {
         require('catppuccin').setup({
             custom_highlights = function(colors)
                 return {
-                    Normal     = { fg = 'none', bg = 'gray4' },
-                    NormalNC   = { fg = 'none', bg = 'gray8' },
-                    TabLineSel = { fg = 'none', bg = 'gray4' },
-                    TabLine    = { fg = 'none', bg = 'gray24' },
+                    Normal                 = { fg = 'none', bg = 'gray4' },
+                    NormalNC               = { fg = 'none', bg = 'gray8' },
+                    TabLineSel             = { fg = 'none', bg = 'gray4' },
+                    TabLine                = { fg = 'none', bg = 'gray24' },
 
                     -- Diff colors
-                    DiffAdd    = { fg = "none", bg = "#115522" },
-                    DiffChange = { fg = "none", bg = "#112233" },
-                    DiffDelete = { fg = "none", bg = "#551144" },
-                    DiffText   = { fg = 'none', bg = '#2222aa', bold = true },
+                    DiffAdd                = { fg = "none", bg = "#113311" },
+                    DiffChange             = { fg = "none", bg = "#112244" },
+                    DiffDelete             = { fg = "none", bg = "#551144" },
+                    DiffText               = { fg = 'none', bg = '#2222aa', bold = true },
+
+                    -- Git colors
+                    GitSignsAddInline      = { fg = "none", bg = "red" },
+                    GitSignsDeleteInline   = { fg = "none", bg = "red" },
+                    GitSignsChangeInline   = { fg = "none", bg = "red" },
+
+                    GitSignsAddLnInline    = { fg = "none", bg = "red" },
+                    GitSignsDeleteLnInline = { fg = "none", bg = "red" },
+                    GitSignsChangeLnInline = { fg = "none", bg = "red" },
+
+                    GitSignsStagedAdd      = { fg = "none", bg = "green" },
+                    GitSignsStagedAddLn    = { fg = "none", bg = "green" },
+                    GitSignsStagedChange   = { fg = "none", bg = "blue" },
+                    GitSignsStagedChangeLn = { fg = "none", bg = "blue" },
+                    GitSignsStagedDelete   = { fg = "none", bg = "red" },
+                    GitSignsStagedDeleteLn = { fg = "none", bg = "red" }
                 }
             end
         })
