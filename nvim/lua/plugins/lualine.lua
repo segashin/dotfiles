87bb2a02f23d1bc5a3ca40b7f9105296bc6ac2a7
Shinsaku <segashin0810shin@outlook.jp>
2025-12-07 21:45:22 +0900

    Update colors

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -5,7 +5,7 @@ return {
         local lualine = require('lualine')
         lualine.setup({
             options = {
-                theme = 'papercolor_dark',
+                theme = 'palenight',
                 component_separators = { left = '', right = '|' },
                 section_separators = { left = '', right = '' },
                 -- component_separators = { left = '', right = ''},
