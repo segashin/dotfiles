d064060ef5dbeade6b752b6d7a77ca3d2f9fade3
Shinsaku <segashin0810shin@outlook.jp>
2025-12-07 15:54:18 +0900

    Fix typo

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -5,7 +5,7 @@ return {
         local lualine = require('lualine')
         lualine.setup({
             options = {
-                theme = 'papaercolor_dark',
+                theme = 'papercolor_dark',
                 component_separators = { left = '', right = '|' },
                 section_separators = { left = '', right = '' },
                 -- component_separators = { left = '', right = ''},
