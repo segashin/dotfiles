4fa42b4a77a3ccbcfea54cb615b843ecb6be2859
Shinsaku <segashin0810shin@outlook.jp>
2025-12-26 13:47:18 +0900

    Customize tokyonight diff colors

--- nvim/lua/plugins/tokyonight.lua
+++ nvim/lua/plugins/tokyonight.lua
@@ -2,4 +2,15 @@ return {
   "folke/tokyonight.nvim",
   lazy = false,
   priority = 1000,
+  config = function()
+      require("tokyonight").setup({
+        style = "night",
+        on_highlights = function(hl, c)
+            hl.DiffAdd                = { fg = "none", bg = "#112a25" }
+            hl.DiffChange             = { fg = "none", bg = "#112244" }
+            -- hl.DiffDelete             = { fg = "none", bg = "#551144" }
+            -- hl.DiffText               = { fg = 'none', bg = '#2222aa', bold = true }
+        end,
+      })
+  end
 }
