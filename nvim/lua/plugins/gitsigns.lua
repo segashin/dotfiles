c0e4487b780a87cb90a4672785b9569c17220d7b
segashin <segashin0810shin@outlook.jp>
2026-01-04 04:43:38 -0500

    Finalize Gitsigns color

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,5 +1,7 @@
 return {
     "lewis6991/gitsigns.nvim",
+    lazy = false,
+    priority = 500,
     config = function()
         local gitsigns = require("gitsigns")
         gitsigns.setup({
@@ -23,13 +25,14 @@ return {
             signcolumn                   = true, -- Toggle with `:Gitsigns toggle_signs`
             numhl                        = true, -- Toggle with `:Gitsigns toggle_numhl`
             linehl                       = true, -- Toggle with `:Gitsigns toggle_linehl`
-            word_diff                    = true, -- Toggle with `:Gitsigns toggle_word_diff`
+            word_diff                    = false, -- Toggle with `:Gitsigns toggle_word_diff`
             diff_opts                    = {
                 internal = true,                 -- Required for word_diff
             },
             watch_gitdir                 = {
-                follow_files = true
+                follow_files = true,
             },
+            debug_mode                   = true,
             auto_attach                  = true,
             attach_to_untracked          = false,
             current_line_blame           = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
@@ -42,10 +45,10 @@ return {
                 use_focus = true,
             },
             current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
-            sign_priority                = 6,
-            update_debounce              = 100,
-            status_formatter             = nil,   -- Use default
-            max_file_length              = 40000, -- Disable if file is longer than this (in lines)
+            sign_priority                = 1000,
+            update_debounce              = 2000,
+            status_formatter             = nil,  -- Use default
+            max_file_length              = 2000, -- Disable if file is longer than this (in lines)
             preview_config               = {
                 -- Options passed to nvim_open_win
                 border = 'single',
@@ -65,8 +68,8 @@ return {
                 map('n', '<leader><Space>', gitsigns.stage_hunk)
                 -- map('n', '<leader>gr', gitsigns.reset_hunk)
                 map('n', '<leader>gl', function()
-                  gitsigns.toggle_linehl()
-                  gitsigns.toggle_word_diff()
+                    gitsigns.toggle_linehl()
+                    gitsigns.toggle_word_diff()
                 end)
                 map('n', '<leader>g;', gitsigns.toggle_deleted)
                 -- map('v', '<leader>vs', function() gitsigns.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
@@ -79,11 +82,10 @@ return {
                 -- map('n', '<leader>vt', gitsigns.toggle_current_line_blame)
                 -- map('n', '<leader>vf', gitsigns.diffthis)
                 -- map('n', '<leader>vF', function() gitsigns.diffthis('~') end)
-                -- map('n', '<leader>vd', gitsigns.toggle_deleted)
 
                 -- Text object
-                map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
-                map('n', '<leader>vw', gitsigns.toggle_linehl)
+                -- map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+                -- map('n', '<leader>vw', gitsigns.toggle_linehl)
             end
         })
     end,
--- nvim/lua/plugins/tokyonight.lua
+++ nvim/lua/plugins/tokyonight.lua
@@ -1,39 +1,47 @@
 return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  config = function()
-      require("tokyonight").setup({
-        style = "night",
-        on_highlights = function(hl, c)
-            hl.Normal                 = { fg = 'none', bg = 'gray8' }
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+        require("tokyonight").setup({
+            style = "night",
+            on_highlights = function(hl, c)
+                hl.Normal                 = { fg = 'none', bg = 'gray8' }
 
-            -- Line colors representing git status
-            -- hl.DiffDelete             = { fg = "none", bg = "#551144" }
-            -- hl.DiffText               = { fg = 'none', bg = '#2222aa', bold = true }
+                -- Line colors representing git status
+                -- hl.DiffDelete             = { fg = "none", bg = "#551144" }
+                -- hl.DiffText               = { fg = 'none', bg = '#2222aa', bold = true }
 
-            -- Add
-            hl.DiffAdd                = { fg = "none", bg = "#222f2f" } -- Unstaged add
-            hl.GitSignsStagedAdd      = { fg = "none", bg = "#223524" } -- Staged add indicator
-            hl.GitSignsStagedAddLn    = { fg = "none", bg = "#223524" } -- Staged add
+                -- Add
+                hl.GitSignsAdd            = { fg = "#225a2f", bg = "none" }     -- Add indicator
+                hl.GitSignsAddLn              = { fg = "none", bg = "#222f2f" } -- Add
+                hl.GitSignsStagedAdd      = { fg = "#22bf2f", bg = "none" }     -- Staged add indicator
+                hl.GitSignsStagedAddLn    = { fg = "none", bg = "#323f54" }     -- Staged add
 
-            -- Change
-            hl.DiffChange             = { fg = "none", bg = "#112244" } -- Unstaged change
-            hl.GitSignsStagedChange   = { fg = "none", bg = "#323f44" } -- Staged change indicator
-            hl.GitSignsStagedChangeLn = { fg = "none", bg = "#323f44" } -- Staged change
+                -- Change
+                hl.GitSignsChange         = { fg = "#2255aa", bg = "none" }     -- Change indicator
+                hl.GitSignsChangeLn           = { fg = "none", bg = "#112244" } -- Change
+                hl.GitSignsStagedChange   = { fg = "#2222ff", bg = "none" }     -- Staged change indicator
+                hl.GitSignsStagedChangeLn = { fg = "none", bg = "#323f54" }     -- Staged change
 
-            -- hl.GitSignsAddInline      = { fg = "none", bg = "red" }
-            -- hl.GitSignsDeleteInline   = { fg = "none", bg = "red" }
-            -- hl.GitSignsChangeInline   = { fg = "none", bg = "red" }
+                -- Delete
+                hl.GitSignsDelete         = { fg = "#662244", bg = "none" }     -- Delete indicator
+                hl.GitSignsDeleteLn           = { fg = "none", bg = "#351144" } -- Delete
+                hl.GitSignsStagedDelete   = { fg = "#ff1144", bg = "none" }     -- Staged delete indicator
+                hl.GitSignsStagedDeleteLn = { fg = "none", bg = "#323f54" }     -- Staged delete
 
-            -- hl.GitSignsAddLnInline    = { fg = "none", bg = "red" }
-            -- hl.GitSignsDeleteLnInline = { fg = "none", bg = "red" }
-            -- hl.GitSignsChangeLnInline = { fg = "none", bg = "red" }
-
-            -- hl.GitSignsStagedDelete   = { fg = "none", bg = "red" }
-            -- hl.GitSignsStagedDeleteLn = { fg = "none", bg = "red" }
-
-        end,
-      })
-  end
+                -- -- Change word diff
+                -- -- Below does not work: https://github.com/lewis6991/gitsigns.nvim/issues/731
+                -- hl.GitSignsChangeLnInline     = { fg = "#0000ff", bg = "#0000ff"}
+                -- hl.GitSignsChangeVirtLnInline = { fg = "#0000ff", bg = "#0000ff"}
+                -- hl.GitSignsChangeInline       = { fg = "#0000ff", bg = "#0000ff"}
+                -- hl.GitSignsAddLnInline        = { fg = "#0000ff", bg = "#0000ff"}
+                -- hl.GitSignsAddVirtLnInline    = { fg = "#0000ff", bg = "#0000ff"}
+                -- hl.GitSignsAddInline          = { fg = "#0000ff", bg = "#0000ff"}
+                -- hl.GitSignsDeleteLnInline     = { fg = "#0000ff", bg = "#0000ff"}
+                -- hl.GitSignsDeleteVirtLnInline = { fg = "#0000ff", bg = "#0000ff"}
+                -- hl.GitSignsDeleteInline       = { fg = "#0000ff", bg = "#0000ff"}
+            end,
+        })
+    end
 }
