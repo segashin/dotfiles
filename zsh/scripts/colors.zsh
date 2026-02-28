ac5d63d6cff2f91f443061a5164667a734df4fdd
segashin <segashin0810shin@outlook.jp>
2026-02-28 09:45:50 -0500

    Enable highlight color

--- zsh/scripts/colors.zsh
+++ zsh/scripts/colors.zsh
@@ -4,7 +4,10 @@ if [[ -n "$TMUX" ]] || [[ "$TERM" == "screen"* ]]; then
 export TERM=screen-256color
 fi
 
-# Remove ls highlight color
-_ls_colors=":ow=01;33" 
-zstyle ':completion:*:default' list-colors "${(s.:.)_ls_colors}"
-LS_COLORS+=$_ls_colors
+# Enable colored ls output
+alias ls='ls --color=auto'
+
+# # Remove ls highlight color
+# _ls_colors=":ow=01;33" 
+# zstyle ':completion:*:default' list-colors "${(s.:.)_ls_colors}"
+# LS_COLORS+=$_ls_colors
